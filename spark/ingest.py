aa91cb6ea7b12c914f7251161889f8e43c8f3fb1
nithinveer <nithinveer@iitj.ac.in>
2021-04-24 17:04:01 +0000

    Spark Parshing

--- spark/ingest.py
+++ spark/ingest.py
@@ -18,19 +18,22 @@ def process_log(fileName):
             "F" : "Others"
     }
     for line in lines:
-            print(line)
+        try:
+#             print(line)
             lineContent = (line.split())
-            print(lineContent)
+#             print(lineContent)
             doc = {}
             day, month, year = lineContent[0].split('/')
             time = lineContent[1]
 
             doc["cateogory"]  = lineContent[2]
             tmp = lineContent[3].replace(':','')
-            if 'Remoting' in tmp:
-                doc["service"], doc["action"] = 'Remoting',''
-            else:
+            if '.' in tmp :
                 doc["service"], doc["action"] = tmp.split('.')
+            else:
+                doc["service"], doc["action"] = tmp,''
+                
+            
 
             doc['timestamp'] = ('20{}-{}-{}T{}Z'.format(year, month, day, time))
     #         if lineContent[4] in log_mapp:
@@ -42,6 +45,8 @@ def process_log(fileName):
     #         doc['log'] = " ".join(lineContent[6:])
             print(doc)
             esInsert(doc, "spark")
+        except Exception as ex:
+            print(ex)
 for root,dirs,files in os.walk('logs/'):
     for file in files:
        if file.endswith(".log"):
